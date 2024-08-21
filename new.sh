3eb2e59f624b75ddf9027da6df3614a9fe360d2f
Peter Christopher Scarlett <scarlettyolo@gmail.com>
2024-08-20 20:14:48 -0500

    new.sh

---
+++ new.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 01 Ops Challenge Solution
+# Author: Peter Scarltt
+# Date of latest revision: 8-19-2024
+# Purpose: Print a string to the terminal
+
+echo "hello"
+echo "class"
+echo "bacon"
---
+++ new2.sh
@@ -0,0 +1,5 @@
+# #!bin/bash
+# Declare a variable with as your name
+# Print to the screen Welcome to the Class Name(variable)
+name="Peter"
+echo "Welcome to the Cybersecurity Ops, $name"
