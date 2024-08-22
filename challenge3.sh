f5f036536bb259c573d5a2b99f4407b708c776cd
Peter Christopher Scarlett <scarlettyolo@gmail.com>
2024-08-21 19:09:04 -0500

    challenge3.sh

---
+++ challenge3.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Condtinal Statment 
+# This ops challenges is about conditinal stamtment of if else statments and how they work
+# We are going to take a varibale with the number and have the computer tell us if its greater than 5 less than 5 or equals 5
+
+number=3
+if [ "$number" -gt 5 ]; then
+    echo "$number is greater than 5."
+elif [ "$number" -lt 5 ]; then
+    echo "$number is less than 5."
+else 
+    echo "$number is exactly 5."
+fi
