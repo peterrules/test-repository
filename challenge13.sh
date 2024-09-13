0274b31933e83ef7aa7e741e364864bccfded21c
Peter Christopher Scarlett <scarlettyolo@gmail.com>
2024-09-12 19:26:11 -0500

    challenge12.sh

---
+++ challenge13.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+# Create a script that you think you would use in a daily job routine No right or Wrong anwsers here
+# You could ping all the devices in your network?
+# Run a script to reset your ip address?
+# Create a script that does some math? 
+
+#A Simple script for fun things
+
+read -p "try something" input
+case $input in
+
+  bacon)
+    echo "≈≈≈≈"    
+
+    ;;
+
+  funkytown)
+    echo "Gotta make a move to a town that's right for me
+Town to keep me movin'
+Keep me groovin' with some energy
+Well, I talk about it, talk about it
+Talk about it, talk about it
+Talk about, talk about
+Talk about movin'
+Gotta move on
+Gotta move on
+Gotta move on
+Won't you take me to
+Funkytown?
+Won't you take me to
+Funkytown?
+Won't you take me to
+Funkytown?
+Won't you take me to
+Funkytown?
+Won't you take me to
+Funkytown?
+Won't you take me to
+Funkytown?
+Won't you take me to
+Funkytown?
+Won't you take me to
+Funkytown?
+Gotta make a move to a town that's right for me
+Town to keep me movin'
+Keep me groovin' with some energy
+Well, I talk about it, talk about it
+Talk about it, talk about it
+Talk about, talk about
+Talk about movin'
+Gotta move on
+Gotta move on
+Gotta move on
+Won't you take me to
+Funkytown?
+Won't you take me to
+Funkytown?
+Won't you take me to
+Funkytown?
+Won't you take me to
+Funkytown?
+Won't you take me down
+To Funkytown?
+Won't you take me down
+To Funkytown?"
+    ;;
+    wassup)
+    echo "wassssssuuuuup"
+    
+    ;;
+
+  *)
+    echo "that doesnt work"
+    ;;
+esac
